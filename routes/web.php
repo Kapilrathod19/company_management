239344192dba27a8eada55dcb272ba1d5cf0fe23
Kapilrathod19 <kapilrathod3456@gmail.com>
2025-11-20 11:17:45 +0530

    remove company login

--- routes/web.php
+++ routes/web.php
@@ -66,27 +66,6 @@ Route::middleware(['auth', 'checkRole:admin'])->group(function () {
     });
 });
 
-Route::middleware(['auth', 'checkRole:company'])->group(function () {
-    Route::prefix('company')->group(function () {
-        Route::get('dashboard', [CompanyDashboardController::class, 'index'])->name('company.dashboard');
-
-        Route::prefix('company_profile')->group(function () {
-            Route::get('/', [CompanyDashboardController::class, 'profile'])->name('company.profile');
-            Route::post('store', [CompanyDashboardController::class, 'profile_store'])->name('company_profile.store');
-            Route::post('change_password', [CompanyDashboardController::class, 'change_password'])->name('company_profile.change_password');
-        });
-
-        Route::prefix('users')->group(function () {
-            Route::get('/', [CompanyUsersController::class, 'users'])->name('company.users');
-            Route::get('create_user', [CompanyUsersController::class, 'create_user'])->name('company.create_user');
-            Route::post('/store', [CompanyUsersController::class, 'store_user'])->name('company.store_user');
-            Route::get('/edit/{id}', [CompanyUsersController::class, 'edit_user'])->name('company.edit_user');
-            Route::put('/update/{id}', [CompanyUsersController::class, 'update_user'])->name('company.update_user');
-            Route::get('/destroy/{id}', [CompanyUsersController::class, 'destroy_user'])->name('company.destroy_user');
-        });
-    });
-});
-
 Route::middleware(['auth:company_user'])->prefix('user')->group(function () {
 
     Route::get('dashboard', [UserController::class, 'index'])->name('user.dashboard');
